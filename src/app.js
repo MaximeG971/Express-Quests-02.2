1be0bfb39dab024957c4792aee8b97a53139d476
MAXIME GAEL <maximejordan.gael@gmail.com>
2023-11-30 09:55:53 +0100

    quete 04 Delete

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.post("/api/movies", movieControllers.postMovie);
 app.put("/api/movies/:id", movieControllers.updateMovie);
+app.delete("/api/movies/:id", movieControllers.deleteMovie);
 
 const userControllers = require("./controllers/userControllers");
 
@@ -20,5 +21,6 @@ app.get("/api/users", userControllers.getUsers);
 app.get("/api/users/:id", userControllers.getUserById);
 app.post("/api/users", userControllers.postUser);
 app.put("/api/users/:id", userControllers.updateUser);
+app.delete("/api/users/:id", userControllers.deleteUser);
 
 module.exports = app;
